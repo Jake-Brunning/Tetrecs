06878232bb8b19a9efd0aa27dfcc34b2485a0b55
Jake Brunning <tarmacshrimp@gmail.com>
2024-04-05 13:54:50 +0100

    Score scene now displays the top 10 scores

--- src/main/java/uk/ac/soton/comp1206/scene/ScoreScene.java
+++ src/main/java/uk/ac/soton/comp1206/scene/ScoreScene.java
@@ -1,36 +1,32 @@
 package uk.ac.soton.comp1206.scene;
 
-import javafx.beans.property.Property;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.util.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.ac.soton.comp1206.component.GameBlock;
+import uk.ac.soton.comp1206.event.CommunicationsListener;
 import uk.ac.soton.comp1206.game.Game;
-import uk.ac.soton.comp1206.game.GamePiece;
 import uk.ac.soton.comp1206.helpers.FileReader;
 import uk.ac.soton.comp1206.helpers.Multimedia;
 import uk.ac.soton.comp1206.helpers.getImage;
+import uk.ac.soton.comp1206.network.Communicator;
 import uk.ac.soton.comp1206.ui.GamePane;
 import uk.ac.soton.comp1206.ui.GameWindow;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Optional;
 
 import static uk.ac.soton.comp1206.helpers.getImage.getImage;
 
@@ -39,19 +35,15 @@ public class ScoreScene extends BaseScene{
     private static final Logger logger = LogManager.getLogger(ScoreScene.class);
     private Game game; //the final gamestate
 
-    //the scores saved on this device
-    private ArrayList<Pair<String, Integer>> localScores;
-    private String[] strLocalScores;
-
-    //the scores saved online
-    private ArrayList<Pair<String, Integer>> onlineScores;
-
-    //SPACES IN NAMES NOT ALLOWED
+    private VBox onlineScoresVbox;
+    private Communicator com;
 
     public ScoreScene(GameWindow gameWindow, Game game){
         super(gameWindow);
         this.game = game;
+        com = gameWindow.getCommunicator();
         logger.info("Creating Scores Scene");
+
     }
 
     @Override
@@ -71,12 +63,16 @@ public class ScoreScene extends BaseScene{
         backImage.setFitHeight(gameWindow.getHeight());
         root.getChildren().add(backImage);
 
-        //vbox to display the scores vertically
-        VBox scoresVBox = createScoresDisplay();
+        //vbox to display the local scores vertically
+        VBox scoresVBox = createScoresDisplay(extractScoresAndNamesAsPair(FileReader.readScoresAndNamesAsString()));
+
+        //vbox to display the online scores vertically
+        onlineScoresVbox = new VBox();
 
         //make borderpane to add vbox
         BorderPane borderPane = new BorderPane();
         borderPane.setLeft(scoresVBox);
+        borderPane.setRight(onlineScoresVbox);
 
         //make text field to enter name
         TextField namegetter = createTextField();
@@ -95,14 +91,17 @@ public class ScoreScene extends BaseScene{
         root.getChildren().add(borderPane);
 
         submitButton.setOnAction(e ->{
-            //on click submit the users score to the score file. Spaces break text file setup so replace with dash
-            FileReader.writeToScoresFile(namegetter.getText().replace(" ", "-") + " " + game.getScore().get());
+            //on click submit the users score to the score file. : break text file setup so replace with dash
+            FileReader.writeToScoresFile(namegetter.getText().replace(":", "-") + ":" + game.getScore().get());
             //make button and namefield disappear
             borderPane.getChildren().remove(hbox);
             //update the score display incase the current players score is in the top 10
             borderPane.getChildren().remove(scoresVBox);
-            borderPane.setLeft(createScoresDisplay());
+            borderPane.setLeft(createScoresDisplay(extractScoresAndNamesAsPair(FileReader.readScoresAndNamesAsString())));
         });
+
+        //load online scores
+        loadOnlineScores();
 
         //play backgroundmusic
         Multimedia.playBackgroundMusic(Multimedia.MUSIC.END);
@@ -112,22 +111,19 @@ public class ScoreScene extends BaseScene{
      * gets all scores saved and creates a vbox displaying the 10 highest scores.
      * @return the vbox containing labels for the 10 highest scores
      */
-    private VBox createScoresDisplay(){
-        //extract the local scores
-        localScores = extractScoresAndNamesAsPair(FileReader.readScoresAndNamesAsString());
-
+    private VBox createScoresDisplay(ArrayList<Pair<String, Integer>> scores){
         //sort the array list so scores appear in order
-        localScores.sort(Comparator.comparing(x -> -x.getValue()));
+        scores.sort(Comparator.comparing(x -> -x.getValue()));
 
         //vbox to display the scores vertically
         VBox scoresVBox = new VBox();
         scoresVBox.setSpacing(10);
         scoresVBox.setMaxHeight(gameWindow.getHeight());
-        scoresVBox.setPadding(new Insets(75, 0, 0, 75)); //set padding so it doesn't hug the border
+        scoresVBox.setPadding(new Insets(50, 0, 0, 50)); //set padding so it doesn't hug the border
 
-        for(int i = 0; i < localScores.size(); i++){
+        for(int i = 0; i < scores.size(); i++){
             //add the new label.
-            scoresVBox.getChildren().add(createLabel(localScores.get(i).getKey() + ": " + localScores.get(i).getValue().toString(), i));
+            scoresVBox.getChildren().add(createLabel(scores.get(i).getKey() + ": " + scores.get(i).getValue().toString(), i));
             //only want to display 10 highest scores. If there's more than 10 scores in the file, break.
             if(i == 9){
                 break;
@@ -145,10 +141,10 @@ public class ScoreScene extends BaseScene{
     public Label createLabel(String name, int colour){
         Label label = new Label(name); //to return
         label.setTextFill(GameBlock.COLOURS[colour + 1]); //set colour. colour 0 is transparent so add 1
-        label.setFont(new Font("Impact", 35)); //set font
+        label.setFont(new Font("Arial", 25)); //set font
         //set shadow because it looks cool
         DropShadow shadow = new DropShadow();
-        //different shadow values for these labels when comparing to the ones on challenge scene because the labes are smaller
+        //different shadow values for these labels when comparing to the ones on challenge scene because the labels are smaller
         shadow.setOffsetX(2);
         shadow.setOffsetY(2);
         shadow.setWidth(15);
@@ -168,16 +164,16 @@ public class ScoreScene extends BaseScene{
 
         for (String line : scoresAndNames) {
             //lines saved like
-            //<name> <score>
-            //a space splits them, so split the line by the space, add first half and second half to pair
-            toReturn.add(new Pair<>(line.split(" ")[0], Integer.parseInt(line.split(" ")[1])));
+            //<name>:<score>
+            //a : splits them, so split the line by the space, add first half and second half to pair
+            toReturn.add(new Pair<>(line.split(":")[0], Integer.parseInt(line.split(":")[1])));
         }
 
         return toReturn;
     }
 
     private void intiliseKeyboardInputs(){//intilise the events for the keyboard inputs
-        //here so you can get back to the menu
+        //here, so you can get back to the menu
         getScene().addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
             if(key.getCode() == KeyCode.ESCAPE){
                 logger.info("Escape key press detected");
@@ -188,20 +184,49 @@ public class ScoreScene extends BaseScene{
     }
 
     /**
-     * get name from the user
-     * @return the users name
+     * request online scores
      */
-    private String getNameUsingTextInputDialog(){
-        //get the name
-        TextInputDialog nameGetter = new TextInputDialog();
-        nameGetter.setTitle("Enter a Name");
-        nameGetter.setHeaderText("Enter your username!");
-        nameGetter.setContentText("Name");
-
-        //return the name. If there is a space in the name replace it with a dash
-        Optional<String> result = nameGetter.showAndWait();
-        return result.map(s -> s.replace(" ", "-")).orElse("Unknown");
+    private void loadOnlineScores(){
+        com.addListener(this::recieveOnlineScores);
+        com.send("HISCORES");
     }
+
+
+    /**
+     * handle online scores
+     */
+    private void recieveOnlineScores(String response){
+        //remove the hiscores tag at the front of the response
+        //platform.runlater so no weirdness with threads
+        Platform.runLater(() ->{
+            logger.info("Recieved communiication");
+            addOnlineScoreToDisplay(response);
+        });
+
+    }
+
+    /**
+     * add the online scores to the display
+     * @param message the online scores
+     */
+
+    private void addOnlineScoreToDisplay(String message){
+        logger.info("Handling communication");
+        //format the message
+        message = message.replace("HISCORES ", "");
+        VBox temp = createScoresDisplay(extractScoresAndNamesAsPair(message.split("\n")));
+
+        onlineScoresVbox.setSpacing(10);
+        onlineScoresVbox.setMaxHeight(gameWindow.getHeight());
+        onlineScoresVbox.setPadding(new Insets(50, 50, 50, 50)); //set padding so it doesn't hug the border
+
+        //add the message to the display
+        onlineScoresVbox.getChildren().addAll(temp.getChildren());
+
+    }
+
+
+
 
     private TextField createTextField(){
         TextField textField = new TextField();
@@ -225,4 +250,7 @@ public class ScoreScene extends BaseScene{
         button.setOnMouseExited(mouseEvent -> button.setTextFill(Color.GREEN));
         return button;
     }
+
+
+
 }
